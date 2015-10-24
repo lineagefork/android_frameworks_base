df15714aadfd380af8973617a03480c5829d4578
beanstown106 <nbenis106@gmail.com>
2015-10-23 21:07:35 -0400

    Disable Lockscreen Media Art [1/2]
     * toggle to choose whether or not to show album art/media art on lockscreen
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3777,6 +3777,12 @@ public final class Settings {
         public static final String FORCE_EXPANDED_NOTIFICATIONS = "force_expanded_notifications";
 
         /**
+         * Whether to show media art on lockscreen
+         * @hide
+         */
+        public static final String LOCKSCREEN_MEDIA_METADATA = "lockscreen_media_metadata";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
