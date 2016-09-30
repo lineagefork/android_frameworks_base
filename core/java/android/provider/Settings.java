47b870844b78b4cfb60144e71dc06274224204be
Alexander Martinz <eviscerationls@gmail.com>
2016-09-30 20:22:21 +0200

    Add Haptic Feedback to QS tiles [1/2]
    Change-Id: Ib51c183d4afa6ebfca584c5fd1c79d5a0c40aae0
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3710,6 +3710,13 @@ public final class Settings {
          public static final String VOLUME_KEY_CURSOR_CONTROL = "volume_key_cursor_control";
 
         /**
+         * Whether to vibrate when quick settings tile is pressed.
+         *
+         * @hide
+         */
+        public static final String QUICK_SETTINGS_TILES_VIBRATE = "quick_settings_vibrate";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
